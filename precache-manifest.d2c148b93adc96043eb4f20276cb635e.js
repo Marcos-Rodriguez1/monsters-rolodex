deb66aaf926439d72faf2ccec151d4e2660d4c8c
Marcos Rodriguez <marcos.rodriguez@student.csulb.edu>
2020-10-02 16:02:58 -0700

    Updates

---
+++ precache-manifest.d2c148b93adc96043eb4f20276cb635e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "093c0c0f8ddc14c85408f659c69c2432",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d08ee367f7da085dced8",
+    "url": "/monsters-rolodex/static/css/main.8f16dc3f.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d08ee367f7da085dced8",
+    "url": "/monsters-rolodex/static/js/main.34123b4a.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
